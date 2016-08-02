0ab1259a79bf092bccd60a6fcecd26dedbeb3d3e
Edparra <Edprr4@gmail.com>
2016-08-02 10:42:51 -0400

    working very basic

--- java.js
+++ java.js
@@ -1,53 +1,99 @@
-$("h1").css('color', 'red');
+$("h1").css('color', 'white');
+$("h2").css('color', 'purple');
 
 var game = {
           round: 0,
-          active:false,
           side:'.side',
           compyClicks : [],
-          playerClicks : [],
+       //   copy : [],
+          counter : 0,
+        //  clipper : [],
+
+start: function(){
+  this.compyClicks=[];
+  // copy=[];
+  this.round=0;
+  this.newRound();
+},
+
+newRound: function() {
+$('[data-round]').text(++this.round);
+this.randoPad();
+this.displayLevel();
 
 
+},
 
+flash :function($element) {
+    $element.addClass('opClass');
+    var clip = $element.attr('data-tile');
 
-flash :function(element) {
-
-
-    $(element).addClass('opClass');
-    var clip = $(element).attr('data-tile')
+    console.log('clip:', clip);
     $('#beep'+clip)[0].play();
-
+  //  game.clipper.push(clip);
+ // console.log('TODO: make flash happen for element:', element);
     setTimeout(function()
                {
-                $(element).removeClass('opClass')
+                $element.removeClass('opClass')
              }
              , 500
              );
+},
+
+
+/*compyBoomin: function(){
+  $('#beep'+flash.clip)[0].play();
+},*/
+randoPad: function() {
+   this.compyClicks.push(Math.floor(Math.random() * 4) + 1);
+   console.log('compyClicks:', this.compyClicks);
+   this.showSequence();
+},
+checkClicks: function(element){
+   var clip = element.attr('data-tile');
+  if (clip != this.compyClicks[this.counter]){
+    this.round=0;
+    this.compyClicks=[];
+    this.counter=0;
+    alert("Nope! You're wrong");
+ }
+ else if (this.compyClicks.length - 1 == this.counter){
+  this.newRound();
+  this.counter=0;
+ }
+ else {
+  this.counts()
+ }
 
 },
-randoPad: function(passes) {
-          for(i=0; i<passes; i++){
-            this.compyClicks.push(Math.floor(Math.random() * 4) + 1);
-          }
+
+counts: function(){
+  this.counter++;
 },
+
+showSequencePad: function (pad, index) {
+  console.log('showSequencePad called:', pad, index);
+  var that = this;
+  setTimeout(function() {
+    that.flash($('.side' + pad));
+  }, 1000 * (index));
+},
+
 showSequence: function(){
   var that=this;
-  $.each(this.compyClicks, function(index, num){
-    setTimeout(function(){
-      that.flash($(that.side+num));
-},500*index);
-  });
+  this.compyClicks.forEach(function(pad, index) {
+    that.showSequencePad(pad, index);
+  }
+  );
 },
 displayLevel: function(){
   $('.round h2').text('Round:' +this.round);
 },
 
-
 }
 
-function rando (){
-return Math.floor((Math.random() * 4) + 1);
-}
+
+
 
 
 
